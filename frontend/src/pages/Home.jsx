401ec321ab0fecbc6d1fb456af73a3a9bfcadfee
FarSpawn <abhijitabhi127@gmail.com>
2025-03-08 23:59:01 +0530

    ux update

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ export default function Home() {
     return (
       <div className="flex items-center justify-center min-h-screen">
         <h1 className="text-4xl font-bold">Welcome to Vice of Dice</h1>
+        <h4 className="text-2xl font-bold">Please Register to Play the Game</h4>
       </div>
     );
   }
