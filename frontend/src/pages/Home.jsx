7ee253ea95a088a7b7d195b992d1e5214a99cf72
FarSpawn <abhijitabhi127@gmail.com>
2025-03-09 00:01:51 +0530

    silly mistake correction

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,8 @@
 export default function Home() {
     return (
-      <div className="flex items-center justify-center min-h-screen">
+      <div className="flex flex-col items-center justify-center min-h-screen">
         <h1 className="text-4xl font-bold">Welcome to Vice of Dice</h1>
-        <h4 className="text-2xl font-bold">Please Register to Play the Game</h4>
+        <h4 className="mt-[5vh] text-2xl ">Please Register to Play the Game</h4>
       </div>
     );
   }
